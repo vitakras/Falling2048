8be5f1a021219a682567e0cb635c56d413921810
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2017-12-10 10:59:59 -0500

    Create a scriptable object to keep track of the tile style

--- Assets/ThreeOfBricks/Scripts/GameTile.cs
+++ Assets/ThreeOfBricks/Scripts/GameTile.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class GameTile : MonoBehaviour {
+
+    public int number;
     public Image background;
     public Text score;
 
-    public void SetScore(int score) {
-        this.score.text = string.Format("{0}",score);
-    }
-
-    public void SetColor(Color color) {
-        background.color = color;
+    public int Number {
+        get {
+            return number;
+        }
+        set {
+            number = value;
+            this.score.text = string.Format("{0}", number);
+        }
     }
 
     public void SetBackground(Sprite sprite) {
         background.sprite = sprite;
+    }
+    public void SetBackgroundColor(Color color) {
+        background.color = color;
+    }
+
+
+    public void SetTextColor(Color color) {
+        this.score.color = color;
     }
 }
---
+++ Assets/ThreeOfBricks/Scripts/ScriptableObjects/TileStyles.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class TileStyle {
+    public int number;
+    public Color tileColor;
+    public Color textColor;
+}
+
+[CreateAssetMenu(fileName = "tilestyles", menuName = "ThreeOfBlocks/TileStyles", order = 1)]
+public class TileStyles : ScriptableObject {
+
+    public TileStyle[] tilesStyleList;
+    public TileStyle defaultStyle;
+    public Sprite background;
+
+    private Dictionary<int, TileStyle> styles;
+
+    public void Awake() {
+        styles = new Dictionary<int, TileStyle>();
+        foreach (TileStyle style in tilesStyleList) {
+            styles.Add(style.number, style);
+        }
+    }
+
+    public void ApplyStyle(GameTile tile) {
+        TileStyle style = defaultStyle;
+        if (!styles.TryGetValue(tile.number, out style)) {
+            style = defaultStyle;
+        }
+
+        ApplyTileStyle(style, tile);
+    }
+
+    private void ApplyTileStyle(TileStyle style, GameTile tile) {
+        tile.SetBackground(background);
+        tile.SetBackgroundColor(style.tileColor);
+        tile.SetTextColor(style.textColor);
+    }
+}
