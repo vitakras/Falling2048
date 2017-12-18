63afa8fa758b0e8365dc6996049fcd39ae6fc12d
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2017-12-18 17:32:48 -0500

    Created a Simple touch input handler

---
+++ Assets/ThreeOfBricks/Scripts/InputHandler/TouchInputHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchInputHandler : MonoBehaviour, IDragHandler, IPointerDownHandler {
+
+    public float offset;
+    private Vector2 previousPosition;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        float difference = previousPosition.x - eventData.position.x;
+        if (offset < Mathf.Abs(difference)) {
+            previousPosition = eventData.position;
+            if (difference < 0) {
+                Debug.Log("Move " + Direction.right);
+            } else {
+                Debug.Log("Move " + Direction.left);
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("On Pointer down");
+        previousPosition = eventData.position;
+    }
+}
